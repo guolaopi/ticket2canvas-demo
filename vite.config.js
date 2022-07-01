2ef2660818aecfbcdbb6373b2f5f366dedabcad0
guolaopi <guolaopi@gmail.com>
2022-07-01 15:49:01 +0800

    eidt vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    base: "./",
     plugins: [Vue()],
+    build: {
+        outDir: "docs",
+    },
 });
